4e6d6bc63e00e97542201c2bb76dc29094b8a145
Psystec <Psystec@PSYSTEC-PC>
2024-08-26 13:12:52 +0200

    Updated Version

--- Once Human Midi Maestro/FormMain.cs
+++ Once Human Midi Maestro/FormMain.cs
@@ -10,7 +10,6 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using WindowsInput.Native;
 using System.Text;
-using System.Net;
 
 namespace Once_Human_Midi_Maestro
 {
@@ -27,7 +26,7 @@ namespace Once_Human_Midi_Maestro
         public FormMain()
         {
             InitializeComponent();
-            this.Text = "Once Human MIDI Maestro by Psystec v2.7.3";
+            this.Text = "Once Human MIDI Maestro by Psystec v3.0.0";
             InitializeMidiInput();
 
             MidiKeyMap.LoadFromJson("MidiKeyMap.json");
